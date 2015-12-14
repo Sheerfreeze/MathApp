63b2b154560add8469a412ea4fd4908de11f2a38
Brooks J Rady <b.j.rady@gmail.com>
2015-12-14 13:06:15 -0700

    Add substitution function

--- Language/Math/Algebra/Types.hs
+++ Language/Math/Algebra/Types.hs
@@ -2,26 +2,40 @@ module Language.Math.Algebra.Types (
     Polarity,
     Coefficient,
     Degree,
-    Base(..),
+    Base,
     Operation(..),
-    LinkingOp,
+    Polynomial(..),
     Formula(..),
     Equation
 ) where
+
+import Data.Maybe
 
 type Polarity = Bool
 type Coefficient = Double
 type Degree = Double
 
-data Operation = Add | Subtract | Multiply | Divide deriving (Show, Eq)
-type LinkingOp = Operation
+data Operation = Add | Subtract | Multiply | Divide | Power deriving (Show, Eq)
+data Polynomial = Head | LinkingOp Operation | Component Formula deriving (Show, Eq)
 
-data Base = Var Char | Val Double deriving (Show, Eq)
+type Base = Maybe Char
 
-data Formula = Mono {polarity :: Polarity,
-                     coefficient :: Coefficient,
-                     base :: Base,
-                     degree :: Degree} |
-               Group [(LinkingOp, Formula)] deriving (Show, Eq)
+data Formula = Val Double |
+               Var Char |
+               Group [Polynomial] deriving (Eq)
+
+instance Show Formula where
+    show (Val x) = show x
+    show (Var x) = [x]
+    show (Group []) = ")"
+    show (Group (Head:xs)) = "(" ++ show (Group xs)
+    show (Group ((Component part):xs)) = (show part) ++ show (Group xs) --Eventually only wrap ends and beginnings in ()
+    show (Group ((LinkingOp lOp):xs)) = case lOp of
+            Add -> "+" ++ show (Group xs)
+            Subtract -> "-" ++ show (Group xs)
+            Multiply -> "*" ++ show (Group xs)
+            Divide -> "/" ++ show (Group xs)
+            Power -> "^" ++ show (Group xs)
+
 
 type Equation = (Formula, Ordering, Formula)
--- Language/Math/Algebra/Utils.hs
+++ Language/Math/Algebra/Utils.hs
@@ -1,8 +1,23 @@
 module Language.Math.Algebra.Utils (
+    merge,
+    substitution,
     clearScreen
 ) where
 
 import Control.Monad
+import Language.Math.Algebra.Types
+
+merge [] ys = ys
+merge (x:xs) ys = x:merge ys xs
+
+substitution :: Formula -> Char -> Formula -> Formula
+substitution (Var i) v r = if (i == v)
+    then r
+    else Var i
+substitution (Val i) _ _ = Val i
+substitution (Group lst) v r  = Group $ Head : merge (map (\ (Component x) -> Component $ substitution x v r) comps) ops
+    where comps = [Component x | Component x <- lst]
+          ops = [LinkingOp x | LinkingOp x <- lst]
 
 clearScreen :: IO ()
 clearScreen = replicateM_ 100 (putStrLn "")
